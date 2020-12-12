1266f374a01cdd525862f56e40f72909e6e41041
PHMS <36188023+Eternalinfinity@users.noreply.github.com>
2020-12-12 10:58:22 +0800

    Add files via upload

---
+++ tts.py
@@ -0,0 +1,11 @@
+import requests
+import sys
+import webbrowser
+while True:
+    a = input("Type your text here:     ")
+    b = input("Chose your preferred language here e.g. EN for English:       ")
+    r1 = requests.get(url='https://tts.baidu.com/text2audio?', params={'tex':a, 'cuid':'baike','lan':b,'ctp':'1' ,'pdt':'1', 'vol':'9',
+    'rate':'32' , 'per':'8', 'qq-pf-to':'pcqq.c2c'})     
+    #print(r1.url)
+    webbrowser.open(r1.url, new=0, autoraise=False) 
+
